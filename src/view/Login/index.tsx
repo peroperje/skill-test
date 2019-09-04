02188395ea5c7e90cac79803169f24c7a7b21737
peroperje <el.pionir@gmail.com>
2019-09-04 09:22:37 +0200

    base login component

--- src/view/Login/index.tsx
+++ src/view/Login/index.tsx
@@ -1,5 +1,56 @@
 import React from 'react';
+import { Formik, FormikProps } from 'formik';
 
-const Login: React.FC = (): JSX.Element => <>Login page</>;
+export interface FormValues {
+  email: string;
+  password: string;
+}
+
+const initialValues: FormValues = {
+  email: '',
+  password: ''
+};
+
+const Login: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <Formik
+        onSubmit={(values: FormValues): void => {
+          console.log(values);
+        }}
+        initialValues={initialValues}
+      >
+        {({
+          values,
+          handleSubmit,
+          handleChange,
+          handleBlur
+        }: FormikProps<FormValues>): JSX.Element => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <div className="Flex-column">
+                <input
+                  name="email"
+                  type="text"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <input
+                  name="password"
+                  type="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          );
+        }}
+      </Formik>
+    </>
+  );
+};
 
 export default Login;
