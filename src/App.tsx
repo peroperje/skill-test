b52a8617474d28352ecc453040cddd0979f74114
peroperje <el.pionir@gmail.com>
2019-09-05 11:11:28 +0200

    installed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 import './App.css';
+import state from './state';
 import {
   ROOT_ROUTE,
   LOGIN_ROUTE,
@@ -13,22 +15,24 @@ import Login from './view/Login';
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <div className="Flex-row">
-            <Link to={ROOT_ROUTE}>Home</Link>
-            <Link to={FILES_ROUTE}>File Manager</Link>
-            <Link to={LOGIN_ROUTE}>Sign in</Link>
-          </div>
-          <div>
-            <Route path={ROOT_ROUTE} exact component={Home} />
-            <Route path={LOGIN_ROUTE} exact component={Login} />
-            <Route path={FILES_ROUTE} exact component={Files} />
-          </div>
-        </header>
-      </div>
-    </BrowserRouter>
+    <Provider store={state}>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <div className="Flex-row">
+              <Link to={ROOT_ROUTE}>Home</Link>
+              <Link to={FILES_ROUTE}>File Manager</Link>
+              <Link to={LOGIN_ROUTE}>Sign in</Link>
+            </div>
+            <div>
+              <Route path={ROOT_ROUTE} exact component={Home} />
+              <Route path={LOGIN_ROUTE} exact component={Login} />
+              <Route path={FILES_ROUTE} exact component={Files} />
+            </div>
+          </header>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
