709b01377e2a057a104b98f09078ac320eb43f77
peroperje <el.pionir@gmail.com>
2019-09-06 15:48:36 +0200

    files sagas definition

--- src/state/files/sagas.ts
+++ src/state/files/sagas.ts
@@ -1,9 +1,21 @@
 import { SagaIterator } from 'redux-saga';
 import { takeLatest, takeEvery, call, put } from 'redux-saga/effects';
+import { PayloadAction, TypeConstant } from 'typesafe-actions';
 
 import { FilesActionTypes, FileItem } from './types';
-import { fetchFiles as fetchFilesService } from './services';
-import { fetchFilesSuccess, fetchFilesFailed } from './actions';
+import {
+  fetchFiles as fetchFilesService,
+  uploadFile as uploadFileService,
+  deleteFile as deleteFileService
+} from './services';
+import {
+  fetchFilesSuccess,
+  fetchFilesFailed,
+  uploadFileSuccess,
+  uploadFilesFailed,
+  deleteFileSuccess,
+  deleteFileFailed
+} from './actions';
 
 export function* fetch(): SagaIterator {
   try {
@@ -13,11 +25,27 @@ export function* fetch(): SagaIterator {
     yield put(fetchFilesFailed(e.message));
   }
 }
-export function* upload() {
-  yield {};
+export function* upload(
+  action: PayloadAction<TypeConstant, { file: File }>
+): SagaIterator {
+  try {
+    const { payload } = action;
+    const res: unknown = yield call(uploadFileService, payload);
+    yield put(uploadFileSuccess(res as FileItem));
+  } catch (e) {
+    yield put(uploadFilesFailed(e.message));
+  }
 }
-export function* deleteFile() {
-  yield {};
+export function* deleteFile(
+  action: PayloadAction<TypeConstant, number>
+): SagaIterator {
+  try {
+    const { payload } = action;
+    yield call(deleteFileService, payload);
+    yield put(deleteFileSuccess(payload));
+  } catch (e) {
+    yield put(deleteFileFailed(e.message));
+  }
 }
 export default function* watchFile(): SagaIterator {
   yield takeLatest(FilesActionTypes.FETCH_FILES_REQUEST, fetch);
