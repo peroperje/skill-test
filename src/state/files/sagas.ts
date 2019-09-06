da0e8157ef391ab2da7846d6d533946768daff9f
peroperje <el.pionir@gmail.com>
2019-09-06 15:29:29 +0200

    sagas definitions

--- src/state/files/sagas.ts
+++ src/state/files/sagas.ts
@@ -1,1 +1,19 @@
-export {};
+import { SagaIterator } from 'redux-saga';
+import { takeLatest, takeEvery } from 'redux-saga/effects';
+
+import { FilesActionTypes } from './types';
+
+export function* fetch() {
+  yield {};
+}
+export function* upload() {
+  yield {};
+}
+export function* deleteFile() {
+  yield {};
+}
+export default function* watchFile(): SagaIterator {
+  yield takeLatest(FilesActionTypes.FETCH_FILES_REQUEST, fetch);
+  yield takeEvery(FilesActionTypes.UPLOAD_FILE_REQUEST, upload);
+  yield takeEvery(FilesActionTypes.DELETE_FILE_REQUEST, deleteFile);
+}
