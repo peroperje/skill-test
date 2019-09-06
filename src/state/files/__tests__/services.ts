f053ecdaf97b5f77603d86520afcd8fbf7360baf
peroperje <el.pionir@gmail.com>
2019-09-06 15:07:15 +0200

    fetch service test

--- src/state/files/__tests__/services.ts
+++ src/state/files/__tests__/services.ts
@@ -1,1 +1,12 @@
 import { fetchFiles, deleteFile, uploadFile, url } from '../services';
+
+describe('Files Service', () => {
+  it('fetchFiles', done => {
+    fetchFiles().then(res => {
+      expect(res).toEqual({
+        url,
+        method: 'get'
+      });
+    });
+  });
+});
