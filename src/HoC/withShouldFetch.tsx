dd2423c9af4a99b40bd07cf56bf8ea8101584dda
peroperje <el.pionir@gmail.com>
2019-09-06 20:28:55 +0200

    shouldFetchHOC definition

---
+++ src/HoC/withShouldFetch.tsx
@@ -0,0 +1,22 @@
+import React, { ElementType, useEffect } from 'react';
+
+const withShouldFetch = (comparePropName: string, fetchFnPropsName: string) => {
+  return (Component: ElementType): ElementType => {
+    const ShouldFetch: React.FC<any> = props => {
+      const {
+        [comparePropName]: compare,
+        [fetchFnPropsName]: fetchFn,
+        ...rest
+      } = props;
+
+      useEffect(() => {
+        fetchFn();
+      });
+      return <Component {...rest} />;
+    };
+
+    return ShouldFetch;
+  };
+};
+
+export default withShouldFetch;
--- src/view/Files/index.ts
+++ src/view/Files/index.ts
@@ -1,4 +1,42 @@
-import { connect } from 'react-redux';
+import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux';
+import { compose } from 'recompose';
+
+import withShouldFetch from '../../HoC/withShouldFetch';
+import { ApplicationState } from '../../state';
+import { fetchFilesRequest } from '../../state/files/actions';
+import { shouldFetch } from '../../state/files/selectors';
+
 import Files from './Files';
 
-export default connect()(Files);
+interface DispatchProps {
+  fetchFn: () => void;
+}
+
+interface StateProp {
+  shouldFetch: boolean;
+}
+
+const mapStateToProps: MapStateToProps<
+  StateProp,
+  null,
+  ApplicationState
+> = state => ({
+  shouldFetch: shouldFetch(state)
+});
+
+const dispatchStateToProps: MapDispatchToProps<
+  DispatchProps,
+  null
+> = dispatch => ({
+  fetchFn: (): void => {
+    dispatch(fetchFilesRequest());
+  }
+});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    dispatchStateToProps
+  ),
+  withShouldFetch('shouldFetch', 'fetchFn')
+)(Files);
