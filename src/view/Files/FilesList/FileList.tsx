f1265d1b59aa4f596d3dab7883876ec6fd42545e
peroperje <el.pionir@gmail.com>
2019-09-07 17:53:03 +0200

    base table implementation

--- src/view/Files/FilesList/FileList.tsx
+++ src/view/Files/FilesList/FileList.tsx
@@ -4,35 +4,6 @@ import { FileListProps } from './types';
 import { Table, TableRow, TableHead, TableCell, TableBody } from '../../../UI';
 
 import ActionsBtns from './ActionsBtns/ActionsBtns';
-/*
-const FilesList: React.FC<FileListProps> = ({
-  files
-}: FileListProps): JSX.Element => {
-  const isFileArrEmpty = files.length === 0;
-  return (
-    <>
-      {isFileArrEmpty && 'No files'}
-      {!isFileArrEmpty && (
-        <div className="Flex-column" style={{ width: '600px' }}>
-          <div className="Flex-row">
-            <div>File name</div>
-            <div>Uploaded on</div>
-            <div>Action</div>
-          </div>
-          {files.map(({ id, name, updateAt, download }) => {
-            return (
-              <div key={id} className="Flex-row">
-                <div>{name}</div>
-                <div>{updateAt}</div>
-                <ActionsBtns id={id} download={download} />
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </>
-  );
-};*/
 
 const FilesList: React.FC<FileListProps> = ({
   files
@@ -44,7 +15,7 @@ const FilesList: React.FC<FileListProps> = ({
           <TableRow>
             <TableCell>File Name</TableCell>
             <TableCell>Upload On</TableCell>
-            <TableCell></TableCell>
+            <TableCell> </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
