9133ad4f025b6995a47411fc971516a2425950d6
peroperje <el.pionir@gmail.com>
2019-09-07 23:27:48 +0200

    removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import './App.css';
 import state from './state';
 import { ROOT_ROUTE, LOGIN_ROUTE } from './utils/route/routesDefinition';
 import { Container, Box } from './UI';
