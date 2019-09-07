65a4cd89354a1f58fa33ce41dea418c5a7171a9c
peroperje <el.pionir@gmail.com>
2019-09-07 02:45:18 +0200

    disable lint

--- src/HoC/withShouldFetch.tsx
+++ src/HoC/withShouldFetch.tsx
@@ -2,6 +2,7 @@ import React, { ElementType, useEffect } from 'react';
 
 const withShouldFetch = (comparePropName: string, fetchFnPropsName: string) => {
   return (Component: ElementType): ElementType => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const ShouldFetch: React.FC<any> = props => {
       const {
         [comparePropName]: compare,
