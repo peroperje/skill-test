79c5efe76d62ee1aa13aa9c69be966c0d111bcd9
peroperje <el.pionir@gmail.com>
2019-09-07 23:22:32 +0200

    added to table sticky header

--- src/view/Files/FilesList/FileList.tsx
+++ src/view/Files/FilesList/FileList.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import { FileListProps } from './types';
 
-import { Table, TableRow, TableHead, TableCell, TableBody } from '../../../UI';
+import {
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  createStyles,
+  makeStyles
+} from '../../../UI';
 
 import ActionsBtns from './ActionsBtns/ActionsBtns';
+
+const useStyle = makeStyles(() => {
+  return createStyles({
+    tableWrapper: {
+      maxHeight: 407,
+      overflow: 'auto'
+    }
+  });
+});
 
 const FilesList: React.FC<FileListProps> = ({
   files
 }: FileListProps): JSX.Element => {
+  const classes = useStyle();
   return (
-    <>
+    <div className={classes.tableWrapper}>
       <Table stickyHeader>
         <TableHead>
           <TableRow>
-            <TableCell>File Name</TableCell>
-            <TableCell>Upload On</TableCell>
-            <TableCell> </TableCell>
+            <TableCell align="center">File Name</TableCell>
+            <TableCell align="center">Upload On</TableCell>
+            <TableCell align="center"> </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {files.map(({ id, name, updateAt, download }) => {
             return (
               <TableRow key={id}>
-                <TableCell>{name}</TableCell>
-                <TableCell>{updateAt}</TableCell>
-                <TableCell>
+                <TableCell align="center">{name}</TableCell>
+                <TableCell align="center">{updateAt}</TableCell>
+                <TableCell align="center">
                   <ActionsBtns id={id} download={download} />
                 </TableCell>
               </TableRow>
@@ -32,7 +50,7 @@ const FilesList: React.FC<FileListProps> = ({
           })}
         </TableBody>
       </Table>
-    </>
+    </div>
   );
 };
 
